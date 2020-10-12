3418bfdabf6ed50812c04cde600f52397f31e60e
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2020-10-12 23:03:07 +0200

    Aggiungere i file di progetto.

---
+++ PriceCrawler/Program.cs
@@ -0,0 +1,34 @@
+﻿using PriceCrawler.Core;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PriceCrawler
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+			ThreadPool.SetMaxThreads(255, 255);
+			ThreadPool.SetMinThreads(255, 255);
+
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+				.MinimumLevel.Override("System", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Warning)
+				.Enrich.FromLogContext()
+				.CreateLogger();
+
+			// // await DistributedSpider.RunAsync();
+			await EPriceSpider.RunAsync();
+
+			Console.WriteLine("Finito!");
+			Console.ReadKey();
+			Environment.Exit(0);
+		}
+    }
+}
